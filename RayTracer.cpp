5a2823b28e8c988a10ffd8dc102a34e789c4afea
GungniR <guozhidong12@126.com>
2020-05-08 14:11:54 +0800

    First part finished

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -26,21 +26,60 @@ vec3 ray_color(const ray& r,const hittable& world, int depth) {
 	return (1.0 - t) * vec3(1, 1, 1) + t * vec3(0.5, 0.7, 1.0);
 }
 
+hittable_list random_scene() {
+	hittable_list world;
+
+	world.add(make_shared<sphere>(vec3(0, -1000, 0), 1000, make_shared<lambertian>(vec3(0.5, 0.5, 0.5))));
+
+	int i = 1;
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			auto choose_mat = random_double();
+			vec3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+			if ((center - vec3(4, 0.2, 9)).length() > 0.9) {
+				if (choose_mat < 0.8) {
+					//diffuse
+					auto albedo = vec3::random() * vec3::random();
+					world.add(make_shared<sphere>(center, 0.2, make_shared<lambertian>(albedo)));
+				}
+				else if (choose_mat < 0.95) {
+					//metal
+					auto albedo = vec3::random(0.5, 1);
+					auto fuzz = random_double(0, .5);
+					world.add(make_shared<sphere>(center, 0.2, make_shared<metal>(albedo, fuzz)));
+				}
+				else {
+					//glass
+					world.add(make_shared<sphere>(center, 0.2, make_shared<dielectric>(1.5)));
+				}
+			}
+		}
+	}
+	world.add(make_shared<sphere>(vec3(0, 1, 0), 1.0, make_shared<dielectric>(1.5)));
+	world.add(make_shared<sphere>(vec3(-4, 1, 0), 1.0, make_shared<lambertian>(vec3(0.4, 0.2, 0.1))));
+	world.add(make_shared<sphere>(vec3(4, 1, 0), 1.0, make_shared<metal>(vec3(0.7, 0.6, 0.5), 0.0)));
+
+	return world;
+}
+
 int main()
 {
-	const int image_width = 2000;
-	const int image_height = 1000;
+	const int image_width = 400;
+	const int image_height = 200;
 	const int samples_per_pixel = 100;
 	const int max_depth = 50;
-
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-	hittable_list world;
-	world.add(make_shared<sphere>(vec3(0, 0, -1), 0.5, make_shared<lambertian>(vec3(0.7,0.3,0.3))));
-	world.add(make_shared<sphere>(vec3(0, -100.5, -1), 100 ,make_shared<lambertian>(vec3(0.8,0.8,0))));
-	world.add(make_shared<sphere>(vec3(1, 0, -1), 0.5, make_shared<metal>(vec3(0.8, 0.6, 0.2)))); 
-	world.add(make_shared<sphere>(vec3(-1, 0, -1), 0.5, make_shared<metal>(vec3(0.8, 0.8, 0.8))));
-	camera cam;
+	
+	auto world = random_scene();
+
+	const auto aspect_ratio = double(image_width) / image_height;
+	vec3 lookfrom(13, 2, 3); 
+	vec3 lookat(0, 0, 0); 
+	vec3 vup(0, 1, 0);  	//vup决定了视角在camera平面上旋转多少度，0，1，0就是人直立，1，0，0就是往右歪头
+	auto dist_to_focus = 10; 
+	auto aperture = 0.1; 
+	camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
 	for (int j = image_height - 1; j >= 0; j--) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
--- material.h
+++ material.h
@@ -24,15 +24,62 @@ public:
 
 class metal : public material {
 public:
-	metal(const vec3& a) :albedo(a) {}
+	metal(const vec3& a,double f) :albedo(a),fuzz(f<1?f:1) {}
 
 	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
 		vec3 reflected = reflect(r_in.direction(), rec.normal);
-		scattered = ray(rec.p, reflected);
+		scattered = ray(rec.p, reflected+fuzz*random_in_unit_sphere());
 		attenuation = albedo;
-		return (dot(scattered.direction(), rec.normal) > 0);
+		return (dot(scattered.direction(), rec.normal) > 0); //this is not simply true due to fuzziness
 	}
 
 public:
 	vec3 albedo;
+	double fuzz;
+};
+
+double schlick(double cosine, double ref_idx) 
+{ 
+	auto r0 = (1 - ref_idx) / (1 + ref_idx); 
+	r0 = r0 * r0; 
+	return r0 + (1 - r0) * pow((1 - cosine), 5); 
+}
+
+class dielectric :public material {
+public:
+	dielectric(double ri) :ref_idx(ri) {}  //etha
+
+	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+		attenuation = vec3(1, 1, 1);
+		double etai_over_etat;
+		if (rec.front_face) {
+			etai_over_etat = 1.0 / ref_idx;
+		}
+		else {
+			etai_over_etat = ref_idx;
+		}
+
+		vec3 unit_direction = unit_vector(r_in.direction());
+		double cos_theta = ffmin(dot(-unit_direction, rec.normal), 1.0);
+		double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+		if (etai_over_etat * sin_theta > 1) {
+			//there's no solution for the theta prime, meaning reflection
+			vec3 reflected = reflect(unit_direction, rec.normal);
+			scattered = ray(rec.p, reflected);
+			return true;
+		}
+
+		double reflect_prob = schlick(cos_theta, etai_over_etat);
+		if (random_double() < reflect_prob) {
+			vec3 reflected = reflect(unit_direction, rec.normal);
+			scattered = ray(rec.p, reflected);
+			return true;
+		}
+		vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
+		scattered = ray(rec.p, refracted);
+		return true;
+	}
+
+public:
+	double ref_idx;
 };
